7b33d6345e8716678cf62080d3ac3f3defe950c6
zd3H04 <zd15shirokuma@gmail.com>
2020-03-02 10:19:30 +0900

    見出しの追加

--- index.php
+++ index.php
@@ -11,6 +11,7 @@
             <h2>
               <a href="<?php the_permalink() ?>"><?php the_title() ?></a>
             </h2>
+            <h2>かきくけこ</h2>
             <p class="postmeta">
               Posted in <?php the_category(", ")?> |
               <?= get_the_date();  ?>
